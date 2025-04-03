d05d5794db413c9d9ddc50e10cc27c54e3b2fc7f
Krypto-2405 <krisengels69@gmail.com>
2025-04-03 20:58:24 +0200

    modified:   Projekte/main.py

--- Projekte/main.py
+++ Projekte/main.py
@@ -1,4 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-print("123")
+URL = 'https://google.com'
+website = requests.get(URL)
+results = BeautifulSoup(website.content, 'html.parser')
+
+blogbeitraege = results.find_all('div', class_='awr')
+
+for blogbeitrag in blogbeitraege:
+    blog_titel = blogbeitrag.find('h2', )
