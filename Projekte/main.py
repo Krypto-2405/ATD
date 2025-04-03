3795f3122ef61356f45204b45f44397a245c7f91
Krypto-2405 <147793125+Krypto-2405@users.noreply.github.com>
2025-04-03 21:17:56 +0200

    Co-authored-by: hochkant <hochkant@users.noreply.github.com>

--- Projekte/main.py
+++ Projekte/main.py
@@ -7,5 +7,6 @@ results = BeautifulSoup(website.content, 'html.parser')
 
 blogbeitraege = results.find_all('div', class_='awr')
 
-for blogbeitrag in blogbeitraege:
-    blog_titel = blogbeitrag.find('h2', )
+for blogbeitrag in blogbeitraege: 
+    blog_titel = blogbeitrag.find('h2', class_='entry-title')
+    print(blog_titel)
