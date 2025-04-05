32dc87b8553c7d4310e4b64176933f6f52215e34
Krypto-2405 <147793125+Krypto-2405@users.noreply.github.com>
2025-04-06 01:36:14 +0200

    modified:   Projekte/Kris/Input/crawler0_script.py 	modified:   Projekte/Kris/Output/article_links.txt 	modified:   Projekte/Kris/Output/audio.html 	modified:   Projekte/Kris/Output/ausland.html 	modified:   Projekte/Kris/Output/auto.html 	modified:   Projekte/Kris/Output/daten-und-visualisierungen.html 	modified:   Projekte/Kris/Output/debatten.html 	modified:   Projekte/Kris/Output/deinspiegel.html 	modified:   Projekte/Kris/Output/deutschland.html 	modified:   Projekte/Kris/Output/extra.html 	modified:   Projekte/Kris/Output/geschichte.html 	modified:   Projekte/Kris/Output/international.html 	modified:   Projekte/Kris/Output/karriere.html 	modified:   Projekte/Kris/Output/klimawandel.html 	modified:   Projekte/Kris/Output/kultur.html 	modified:   Projekte/Kris/Output/leben.html 	modified:   Projekte/Kris/Output/magazine.html 	modified:   Projekte/Kris/Output/meta_output.html 	modified:   Projekte/Kris/Output/nahostkonflikt.html 	modified:   Projekte/Kris/Output/netzwelt.html 	modified:   Projekte/Kris/Output/panorama.html 	modified:   Projekte/Kris/Output/plus.html 	modified:   Projekte/Kris/Output/reporter.html 	modified:   Projekte/Kris/Output/schlagzeilen.html 	modified:   Projekte/Kris/Output/sport.html 	modified:   Projekte/Kris/Output/start.html 	modified:   Projekte/Kris/Output/tests.html 	modified:   Projekte/Kris/Output/ukraine_konflikt.html 	modified:   Projekte/Kris/Output/video.html 	modified:   Projekte/Kris/Output/wirtschaft.html 	modified:   Projekte/Kris/Output/wissenschaft.html

--- Projekte/Kris/Input/crawler0_script.py
+++ Projekte/Kris/Input/crawler0_script.py
@@ -1,7 +1,11 @@
 import subprocess
 import os
 
+# Das Verzeichnis zu den Skripten
 directory = "F:/Prog/ATD/Projekte/Kris/Input"
+
+# Wechsle in das Verzeichnis, in dem die Skripte liegen
+os.chdir(directory)
 
 # Liste der Skripte, die du ausführen möchtest
 scripts = [
@@ -10,10 +14,12 @@ scripts = [
     "crawler3_html_write.py",
     "crawler4_article_links.py",
     "crawler5_article.py",
-
 ]
 
 # Alle Skripte nacheinander ausführen
 for script in scripts:
-    subprocess.run(["python", script])
-    print(f"{script} wurde ausgeführt.")
+    try:
+        subprocess.run(["python", script], check=True)
+        print(f"{script} wurde ausgeführt.")
+    except subprocess.CalledProcessError as e:
+        print(f"Fehler beim Ausführen von {script}: {e}")
